d43b04dd2c7257340e5ea0d8bd7138e2bd8c93af
noepm <pompeyonoe@gmail.com>
2023-01-04 11:00:38 -0600

    se crearon formula para obtener el perimetro y el area de un cuarado y de un triangulo

---
+++ math.js
@@ -0,0 +1,50 @@
+console.group('Cuadrado')
+
+const ladoCaudrado = 5;
+const perimetroCuadrado = ladoCaudrado * 4;
+
+const areaCuadrado = ladoCaudrado * ladoCaudrado;
+
+function calcularCuadrado(lado) { 
+    return {
+        perimetro: lado * 4,
+        area: lado * lado,
+    }
+
+} ;
+
+console.log({
+    ladoCaudrado,
+    perimetroCuadrado,
+    areaCuadrado
+})
+console.groupEnd('Cuadrado')
+
+console.group('Triangulo')
+
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+function calcularTriangulo(lado1, lado2, base, altura) { 
+    return {
+        perimetro: lado1 + lado2 + base,
+        area: (base * altura) / 2,
+    }
+
+} ;
+
+console.log({
+ladoTriangulo1,
+ladoTriangulo2,
+ladoTrianguloBase,
+alturaTriangulo,
+perimetroTriangulo,
+areaTriangulo
+})
+console.groupEnd('Triangulo')
