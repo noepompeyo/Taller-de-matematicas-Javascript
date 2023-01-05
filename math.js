de95567e3bd49e14815dcf0aa6ce563cb114d661
noepm <pompeyonoe@gmail.com>
2023-01-05 08:10:13 -0600

    se añadia una funcion para sacar el area y perimetro del circulo, y se creo la carpeta gitignore

--- math.js
+++ math.js
@@ -48,3 +48,33 @@ perimetroTriangulo,
 areaTriangulo
 })
 console.groupEnd('Triangulo')
+
+
+console.group('circle')
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+const PI = 3.1416;
+
+const circunferencia = diametroCirculo *PI;
+const areaCirculo = (radioCirculo ** 2 ) * PI;
+
+console.log({
+    radioCirculo,
+    diametroCirculo,
+    PI,
+    circunferencia,
+    areaCirculo
+})
+
+function calcularCirculo (radio) {
+    const diametro = radio * 2 ;
+    const radioAlCuadrado = Math.pow(radio, 2)
+
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI
+        }
+}
+console.groupEnd('circle')
+
