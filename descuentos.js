00aac01185d4e587b06e2df461ecd9082659ad05
noepm <pompeyonoe@gmail.com>
2023-01-10 09:33:56 -0600

    Se creo una formula para filtar un objeto con los cupones que se podrian utilizar para el descuento, y se utilizo la propiedad find y filter

--- descuentos.js
+++ descuentos.js
@@ -5,27 +5,71 @@ const pResolt = document.querySelector('#resolt');
 
 btn.addEventListener('click', calcularPrecioConDescuento);
 
-function calcularPrecioConDescuento () {
-    // (p * (100 * D)) / 100
+    // const arrayObjeto = undefined;
 
+    // const couponObj = {
+    //     'mayo': 30,
+    //     'enero': 25,
+    //     'may': 20,
+    // };
+
+    const couponsList = [];
+    couponsList.push({
+        name: "ola",
+        discount: 30,
+    });
+    couponsList.push({
+        name: "cero",
+        discount: 20,
+    });
+
+function calcularPrecioConDescuento () {
     const price = Number(inputPrice.value);
-    const coupon = inputCoupon.value;
-    
+    const coupon = inputCoupon.value;    
+
     if (!price || !coupon) {
         pResolt .innerText = 'Chancla , porfavor rellena el formulario ';
         return;
     };
+    
 
     let discount;
+    function isCouponInArray (couponElement) {
+        return couponElement.name == coupon;
+    }
 
-    if( coupon == 'morta') {
-        discount = 30;
-    } else if ( coupon == 'mayo') {
-        discount = 25;
+    const couponInArray = couponsList.find(isCouponInArray); 
+
+    if (couponInArray){
+        discount = couponInArray.discount;
+        
     } else {
         pResolt.innerText = "el cupon no es valido";
         return;
     }
+    console.log ({
+        coupon,
+        discount,
+        couponInArray,
+        couponsList,
+        
+    })
+
+    // if (couponObj[coupon] ) {
+    //     discount = couponObj[coupon];
+    // }  else {
+    //     pResolt.innerText = "el cupon no es valido";
+    //         return;
+    // }
+
+    // if( coupon == 'morta') {
+    //     discount = 30;
+    // } else if ( coupon == 'mayo') {
+    //     discount = 25;
+    // } else {
+    //     pResolt.innerText = "el cupon no es valido";
+    //     return;
+    // }
     console.log({price, discount})    
 
     const newPrice = (price * (100 - discount)) / 100;
