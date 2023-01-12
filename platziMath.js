3abdfb84eab4b19748da6c0bf1c096501d61ac5f
noepm <pompeyonoe@gmail.com>
2023-01-12 09:36:21 -0600

    se crao la formula para obtener el promedio usando el metodo reduce

--- platziMath.js
+++ platziMath.js
@@ -1,3 +1,5 @@
+// SE creo la formul par ael promedio con un ciclo for
+
 function calcularPromedio(lista) {
 
     sumaLista = 0;
@@ -5,6 +7,26 @@ function calcularPromedio(lista) {
     for (let i = 0; i < lista.length; i++) {
         sumaLista = sumaLista + lista[i]
     }
+
+    const promedio = sumaLista / lista.length;
+    console.log(promedio)
+    return promedio;
+}
+
+
+calcularPromedio([1,2,3,4]);
+
+//SE hara uso del metodo reduce
+
+function calcularPromedio(lista) {
+
+    function sumarTodosElementos (valorAcumulado, nuevoValor) {
+        return valorAcumulado + nuevoValor;
+    }
+
+    const sumaLista = lista.reduce(sumarTodosElementos);
+
+    
 
     const promedio = sumaLista / lista.length;
     console.log(promedio)
